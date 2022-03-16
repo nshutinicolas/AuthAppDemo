2f6099cf87283dcc9e589babb43078be2aec5f7f
nshutinicolas <nshuti.nicolas@yahoo.com>
2022-03-16 19:59:22 +0200

    Initial Commit

---
+++ AuthenticationApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+// ViewController.swift
+// AuthenticationApp
+//
+// Created by Musoni nshuti Nicolas on 16/03/2022
+// Copyright © 2022 GHOST TECHNOLOGIES LLC. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
